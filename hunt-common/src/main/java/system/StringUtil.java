d89673d67ccb6149b32c492252d6ad8e11ab6a31
ouyangan <981017952@qq.com>
2016-10-12 21:44:27 +0800

    树状目录

--- hunt-common/src/main/java/system/StringUtil.java
+++ hunt-common/src/main/java/system/StringUtil.java
@@ -29,4 +29,50 @@ public class StringUtil {
         throwable.printStackTrace(printWriter);
         return "\n" + writer.toString();
     }
+
+
+    public static String formatJson(String jsonStr) {
+        if (null == jsonStr || "".equals(jsonStr)) return "";
+        StringBuilder sb = new StringBuilder();
+        char last = '\0';
+        char current = '\0';
+        int indent = 0;
+        for (int i = 0; i < jsonStr.length(); i++) {
+            last = current;
+            current = jsonStr.charAt(i);
+            switch (current) {
+                case '{':
+                case '[':
+                    sb.append(current);
+                    sb.append('\n');
+                    indent++;
+                    addIndentBlank(sb, indent);
+                    break;
+                case '}':
+                case ']':
+                    sb.append('\n');
+                    indent--;
+                    addIndentBlank(sb, indent);
+                    sb.append(current);
+                    break;
+                case ',':
+                    sb.append(current);
+                    if (last != '\\') {
+                        sb.append('\n');
+                        addIndentBlank(sb, indent);
+                    }
+                    break;
+                default:
+                    sb.append(current);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static void addIndentBlank(StringBuilder sb, int indent) {
+        for (int i = 0; i < indent; i++) {
+            sb.append('\t');
+        }
+    }
 }
--- hunt-system/src/test/java/com/hunt/service/impl/SystemOrganizationServiceImplTest.java
+++ hunt-system/src/test/java/com/hunt/service/impl/SystemOrganizationServiceImplTest.java
@@ -8,10 +8,13 @@ import com.hunt.service.SystemOrganizationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import system.StringUtil;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,6 +28,7 @@ import java.util.List;
 @ContextConfiguration("/spring.xml")
 //@Transactional
 public class SystemOrganizationServiceImplTest {
+    private static Logger log = LoggerFactory.getLogger(SystemOrganizationServiceImplTest.class);
     int count = 0;
     @Autowired
     private SysOrganizationMapper sysOrganizationMapper;
@@ -35,10 +39,11 @@ public class SystemOrganizationServiceImplTest {
         SysOrganizationTree sysOrganizationTree = new SysOrganizationTree();
         SysOrganization sysOrganization = sysOrganizationMapper.selectById(id);
         BeanUtils.copyProperties(sysOrganization, sysOrganizationTree);
+        //第一次进入递归的时候 , 内部生成变量 ,
         List<SysOrganizationTree> treeList = selectChildrenTreeList(id);
         sysOrganizationTree.setChildren(treeList);
         for (int i = 0; i < treeList.size(); i++) {
-            selectSysOrganizationTree(treeList.get(i).getId());
+            sysOrganizationTree.getChildren().set(i, selectSysOrganizationTree(treeList.get(i).getId()));
         }
         return sysOrganizationTree;
     }
@@ -51,18 +56,16 @@ public class SystemOrganizationServiceImplTest {
             BeanUtils.copyProperties(s, sysOrganizationTree);
             childrenTreeList.add(sysOrganizationTree);
         }
-        if (childrenList.size() == 0) {
-            return Collections.emptyList();
-        }
         return childrenTreeList;
     }
 
     @Test
     public void selectChildren() throws Exception {
         SysOrganizationTree tree = selectSysOrganizationTree(1L);
-        System.out.println("--------------json----------");
-        System.out.println(new Gson().toJson(tree));
-        System.out.println("--------------json----------");
+        log.debug("--------------json----------");
+//        log.debug(StringUtil.formatJson("\n" + new Gson().toJson(tree)));
+        log.debug("\n" + new Gson().toJson(tree));
+        log.debug("--------------json----------");
 
     }
 
@@ -77,10 +80,19 @@ public class SystemOrganizationServiceImplTest {
 //        System.out.println(sysOrganization);
         for (int i = 1; i < 20; i++) {
             SysOrganization sysOrganization1 = new SysOrganization();
-            sysOrganization1.setName("深圳xx软件公司湖南分公司长沙分部岳麓区分公司-" + i);
-            sysOrganization1.setDescription("湖南分公司长沙分部岳麓区分公司-" + i);
+            sysOrganization1.setName("湖南分公司研发部基础架构组-" + i);
+            sysOrganization1.setDescription("湖南分公司研发部基础架构组-" + i);
             sysOrganization1.setIsFinal(1);
-            sysOrganization1.setParentId(40L);
+            sysOrganization1.setParentId(59L);
+            sysOrganizationMapper.insert(sysOrganization1);
+            System.out.println(sysOrganization1);
+        }
+        for (int i = 1; i < 20; i++) {
+            SysOrganization sysOrganization1 = new SysOrganization();
+            sysOrganization1.setName("江西分公司研发部基础架构组-" + i);
+            sysOrganization1.setDescription("江西分公司研发部基础架构组-" + i);
+            sysOrganization1.setIsFinal(1);
+            sysOrganization1.setParentId(78L);
             sysOrganizationMapper.insert(sysOrganization1);
             System.out.println(sysOrganization1);
         }
