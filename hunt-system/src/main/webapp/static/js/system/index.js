72bf792b1b47c5e60f419bbeba9e4e76f67a8bd4
ouyangan <981017952@qq.com>
2016-10-14 10:15:17 +0800

    消除tab重复

--- hunt-system/src/main/webapp/static/js/system/index.js
+++ hunt-system/src/main/webapp/static/js/system/index.js
@@ -6,13 +6,19 @@ $(function () {
     $(".tab-btn").click(function () {
         var title = $(this).find('a:first').text();
         var href = $(this).find('a:first').attr("href");
-        $('#tab').tabs('add', {
-            fit: true,
-            title: title,
-            closable: true,
-            href: href,
-        });
-        return false;
+        if ($('#tab').tabs('exists', title)) {
+            $('#tab').tabs('select', title);
+            return false;
+        } else {
+            $('#tab').tabs('add', {
+                fit: true,
+                title: title,
+                closable: true,
+                href: href,
+            });
+            return false;
+        }
+
     })
 
 })
