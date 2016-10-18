a95c901ff987984ff26c1073f941c0cd082bf662
ouyangan <981017952@qq.com>
2016-10-18 17:05:10 +0800

    登录验证

--- hunt-system/src/main/java/com/hunt/system/security/ShiroRealm.java
+++ hunt-system/src/main/java/com/hunt/system/security/ShiroRealm.java
@@ -38,6 +38,12 @@ public class ShiroRealm extends AuthorizingRealm {
     @Autowired
     private SysRolePermissionMapper sysRolePermissionMapper;
 
+    /**
+     * 鉴权信息
+     *
+     * @param principalCollection
+     * @return
+     */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
